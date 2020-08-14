c6b782eeebc7ed4d1e87a1f3cc3a452d51a4218d
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-14 20:38:40 +0530

    Insert many and find

--- app.js
+++ app.js
@@ -1,55 +1,80 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+// Requiring the mongoose package.
+const mongoose = require("mongoose");
 
-// Connection URL
-const url = "mongodb://localhost:27017";
-
-// Database Name
-const dbName = "fruitDB";
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the server
-client.connect(function (err) {
-	assert.equal(null, err);
-	console.log("Connected successfully to server");
-
-	const db = client.db(dbName);
-
-	findDocuments(db, function () {
-		client.close();
-	});
+// Connecting tht database to localhost and port 27017.
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
-const insertDocuments = function (db, callback) {
-	// Get the documents collection
-	const collection = db.collection("fruits");
-	// Insert some documents
-	collection.insertMany(
-		[
-			{ name: "Apple", score: 8, review: "Great Fruit" },
-			{ name: "Orange", score: 6, review: "Kinda Sour" },
-			{ name: "Banana", score: 9, review: "Great Stuff!" },
-		],
-		function (err, result) {
-			assert.equal(err, null);
-			assert.equal(3, result.result.n);
-			assert.equal(3, result.ops.length);
-			console.log("Inserted 3 fruits into the collection");
-			callback(result);
-		}
-	);
-};
+// Creating a schema or a blueprint for our table.
+const fruitSchema = new mongoose.Schema({
+	name: String,
+	rating: Number,
+	review: String,
+});
 
-const findDocuments = function (db, callback) {
-	// Get the documents collection
-	const collection = db.collection("fruits");
-	// Find some document
-	collection.find({}).toArray(function (err, fruits) {
-		assert.equal(err, null);
-		console.log("Found the following records");
-		console.log(fruits);
-		callback(fruits);
-	});
-};
+// Using the schema creating a model
+const Fruit = mongoose.model("Fruit", fruitSchema); // The name Fruit is changed to all lower case and pluralized.
+
+// Create a new Fruit document.
+const fruit = new Fruit({
+	name: "Apple",
+	rating: 7,
+	review: "Keeps doctors away",
+});
+
+const kiwi = new Fruit({
+	name: "Kiwi",
+	rating: 10,
+	review: "Good",
+});
+
+const orange = new Fruit({
+	name: "Orange",
+	rating: 2,
+	review: "I don't like it.",
+});
+
+const banana = new Fruit({
+	name: "Banana",
+	rating: 10,
+	review: "Bahot bada hai!!",
+});
+
+// Fruit.insertMany([kiwi, orange, banana], function (err) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log("Successfull saved all the fruits to fruitDB");
+// 	}
+// });
+
+Fruit.find(function (err, fruits) {
+	if (err) {
+		console.log(err);
+	} else {
+		// console.log(fruits);
+		console.log("Success");
+		fruits.forEach(function (fruit) {
+			console.log("Name: " + fruit.name);
+		});
+	}
+});
+
+// Saving the document.
+// fruit.save();
+
+const peopleSchema = new mongoose.Schema({
+	name: String,
+	age: Number,
+});
+
+const People = mongoose.model("People", peopleSchema);
+
+const people = new People({
+	name: "Sandy",
+	age: 69,
+});
+
+// people.save();
